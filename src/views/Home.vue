3518e0d2020bb6f2b0a2ccdaade00ac1a27b2d71
denny861014 <denny861014@gmail.com>
2022-03-11 11:59:26 +0800

    首頁修改

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-link to="/login">後台登入</router-link> |
+    <router-link to="/user/cart">商品頁面</router-link>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  },
   created () {
     console.log(process.env.VUE_APP_API, process.env.VUE_APP_PATH)
   }
